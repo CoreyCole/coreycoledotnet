f3756e7a565f0c92fa7cab9bd9c40f785c413979
Your Name <coreyleoc@gmail.com>
2017-08-18 01:10:45 -0700

    chore: adds angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// angular material dependencies
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
+// angular material components
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MdToolbarModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    // angular material components
+    MdButtonModule,
+    MdCheckboxModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
